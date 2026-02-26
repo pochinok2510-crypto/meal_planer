92742689e5c0ff289316cc64f90f65a1f138e704
pochinok2510-crypto <pochinok2510@gmail.com>
2026-02-26 10:58:54 +0300

    Update ShoppingListScreen.kt

--- app/src/main/java/com/example/mealplanner/ui/screens/ShoppingListScreen.kt
+++ app/src/main/java/com/example/mealplanner/ui/screens/ShoppingListScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.stickyHeader
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
